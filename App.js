6db74ecf9c52ae4680e7d5c17b09c7fb211fa55e
“Iuliia” <ybe2004ybe@gmail.com>
2024-01-21 20:03:19 +0200

    Exercise complete

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Image, FlatList, ImageBackground, Keyboard } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const buttonPressed = () => {
+    setData([...data, { key: text }]);
+    setText('');
+    Keyboard.dismiss(); 
+  };
+
+  const image = { uri: 'https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA2L2pvYjE1NzJkZXNpZ24tcmVtaXgtMDFhLWQuanBn.jpg' };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <View style={styles.container}>
+        <Text style={styles.titletext}>Great App</Text>
+        <Image
+          style={{ width: 200, height: 150 }}
+          source={{
+            uri: 'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg',
+          }}
+        />
+        <TextInput
+          style={{ width: 240, borderColor: 'gray', borderWidth: 1, margin: 15, height: 40 }}
+          onChangeText={(text) => setText(text)}
+          value={text}
+        />
+        <Button
+          onPress={buttonPressed}
+          title="Press me"
+        />
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Text style={styles.flatListText}>{item.key}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titletext: {
+    fontSize: 28,
+    color: 'red',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    marginTop: 120,
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  flatListText: {
+    fontSize: 16,
+    color: 'black',
+    marginTop: 5,
   },
 });
